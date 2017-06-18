3784f804332fd9e34579ae1a0eaf0b69e909ef0e
Sebastian Freundt <freundt@ga-group.nl>
2017-06-18 03:58:54 +0000

    chore, switch to quantities with visible and hidden liquidity

--- src/plqu_val.h
+++ src/plqu_val.h
@@ -2,20 +2,24 @@
 #if !defined INCLUDED_plqu_val_h_
 #define INCLUDED_plqu_val_h_
 
-typedef _Decimal64 plqu_val_t;
+/* quantities with hidden liquidity*/
+typedef struct {
+	_Decimal64 vis;
+	_Decimal64 hid;
+} plqu_val_t;
 
-#define plqu_val_0	0.dd
+#define plqu_val_0	((plqu_val_t){0.dd, 0.dd})
 
 static inline __attribute__((pure, const)) plqu_val_t
 plqu_val_add(plqu_val_t x, plqu_val_t y)
 {
-	return x + y;
+	return (plqu_val_t){x.vis + y.vis, x.hid + y.hid};
 }
 
 static inline __attribute__((pure, const)) plqu_val_t
 plqu_val_sub(plqu_val_t x, plqu_val_t y)
 {
-	return x - y;
+	return (plqu_val_t){x.vis - y.vis, x.hid - y.hid};
 }
 
 #endif	/* INCLUDED_plqu_h_ */
