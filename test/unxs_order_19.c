06bcf73064d7e360ab49eaf6e0e62bd7fb3950a0
Sebastian Freundt <freundt@ga-group.nl>
2017-06-25 06:41:09 +0000

    test, provide regression cases for unxs_order() with limit orders

---
+++ test/unxs_order_19.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "clob.h"
+#include "unxs.h"
+#include "nifty.h"
+
+
+int
+main(void)
+{
+	unxs_exbi_t x[16U];
+	size_t n;
+	clob_t c;
+
+	c = make_clob();
+
+	clob_add(c, (clob_ord_t){TYPE_LMT, SIDE_ASK, {100.dd, 0.0dd}, .lmt = 200.0dd});
+	clob_add(c, (clob_ord_t){TYPE_LMT, SIDE_BID, {10.dd, 50.0dd}, .lmt = 198.0dd});
+	clob_add(c, (clob_ord_t){TYPE_LMT, SIDE_BID, {10.dd, 50.0dd}, .lmt = 197.0dd});
+
+	clob_prnt(c);
+
+	/* eat into the limits but stop because of buffer constraints */
+	n = unxs_order(x, 2U, c, (clob_ord_t){TYPE_LMT, SIDE_SHORT, {80.dd, 0.0dd}, .lmt = 197.5dd}, NANPX);
+
+	for (size_t i = 0U; i < n; i++) {
+		printf("%f @ %f  %u %u %f %zu  v  %u %u %f %zu\n",
+		       (double)x[i].x.qty, (double)x[i].x.prc,
+		       x[i].o[SIDE_MAKER].typ, x[i].o[SIDE_MAKER].sid, (double)x[i].o[SIDE_MAKER].prc, x[i].o[SIDE_MAKER].qid,
+		       x[i].o[SIDE_TAKER].typ, x[i].o[SIDE_TAKER].sid, (double)x[i].o[SIDE_TAKER].prc, x[i].o[SIDE_TAKER].qid);
+	}
+
+	clob_prnt(c);
+
+	free_clob(c);
+	return n != 2U ||
+		x[0U].x.qty != 20.dd ||
+		x[1U].x.qty != 60.dd || x[1U].x.prc != 198.0dd ||
+		0;
+}
