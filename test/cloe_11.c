7ba772113338bbbec6fba0e935ae2dbee106aa83
Sebastian Freundt <freundt@ga-group.nl>
2017-07-07 05:16:40 +0000

    test, more regression cases for auctioning

---
+++ test/cloe_11.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "clob.h"
+#include "unxs.h"
+#include "mmod-auction.h"
+#include "nifty.h"
+
+
+int
+main(void)
+{
+	clob_t c;
+
+	c = make_clob();
+
+	/* cloe fuckup 1 */
+	clob_add(c, (clob_ord_t){TYPE_MKT, SIDE_BID, {1400.dd, 0.0dd}});
+	clob_add(c, (clob_ord_t){TYPE_LMT, SIDE_ASK, {500.dd, 0.0dd}, .lmt = 2.75});
+	clob_add(c, (clob_ord_t){TYPE_LMT, SIDE_ASK, {500.dd, 0.0dd}, .lmt = 2.80});
+	clob_add(c, (clob_ord_t){TYPE_LMT, SIDE_ASK, {500.dd, 0.0dd}, .lmt = 2.85});
+
+	clob_prnt(c);
+
+	mmod_auc_t auc = mmod_auction(c);
+	printf("AUCTION %f @ %f ~%f\n", (double)auc.qty, (double)auc.prc, (double)auc.imb);
+
+	free_clob(c);
+	return auc.prc != 2.85dd || auc.qty != 1400.dd || auc.imb != -100.dd;
+}
