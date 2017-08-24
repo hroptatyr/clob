8b49b7ee598afe59501132aef120109f52e7ae89
Sebastian Freundt <freundt@ga-group.nl>
2017-08-24 12:09:27 +0000

    minor, test auctions with 10000 orders each

--- test/clob_stress_02.c
+++ test/clob_stress_02.c
@@ -14,14 +14,14 @@ main(void)
 
 	for (size_t i = 0U; i < 10000000U; i++) {
 		clob_add(c, (clob_ord_t){CLOB_TYPE_LMT, rand() % 2, 2 + rand() % 4, 0.dd, .lmt = 100 + rand() % 100});
-		if (!((i - 1U) % 100U)) {
+		if (!((i - 1U) % 10000U)) {
 			mmod_auc_t a = mmod_auction(c);
 			(void)unxs_auction(c, a.prc, a.qty);
 			//clob_prnt(c);
 		}
 	}
 
-	clob_prnt(c);
+	//clob_prnt(c);
 	free_clob(c);
 	return 0;
 }
